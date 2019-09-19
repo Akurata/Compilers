dea6cf6e2d29240fc373cae27a3117904e5db0c3
akurata <alexander.kurata1@marist.edu>
2019-09-19 18:03:43 -0400

    Handlers for lexing finished, error and unknown chars not identified

--- views/js/project_one.js
+++ views/js/project_one.js
@@ -2,7 +2,7 @@
 var lexer_syntax = {
   keywords: {
     priority: 5,
-    values: /\b^print$|\b^while$|\b^if$|\b^int$|\b^string$|\b^boolean$|\b^false$|\b^true$/g
+    values: /\b^print$|\b^while$|\b^if$|\b^int$|\b^string$|\b^boolean$|\b^false$|\b^true$|\b^==$|\b^!=$/g
   },
   id: {
     priority: 4,
@@ -10,7 +10,7 @@ var lexer_syntax = {
   },
   symbol: {
     priority: 3,
-    values: /^\{$|^\}$|^\($|^\)$|^\"$|^\=$|^\!$|^\+$|^\$$|^\/$|^\*$/
+    values: /^\{$|^\}$|^\($|^\)$|^\"$|^\=$|^\!$|^\+$|^\$$/
   },
   digit: {
     priority: 2,
@@ -19,11 +19,31 @@ var lexer_syntax = {
   char: {
     priority: 1,
     values: /^[A-z]$/
+  },
+  comment: {
+    priority: 0,
+    values: /(\/\*).+(\*\/)$/g
   }
 }
 
 var lexer_index = {
-
+  'print': 'PrintStatement',
+  'while': 'WhileStatement',
+  'if': 'IfStatement',
+  '(': 'L_PAREN',
+  ')': 'R_PAREN',
+  '"': 'QUOTE',
+  '*': 'ASTERISK',
+  '=': 'ASSIGN_OP',
+  '==': 'IS_EQUAL',
+  '!=': 'NOT_EQUAL',
+  'int': 'INT_DECL',
+  'string': 'STRING_DECL',
+  'boolean': 'BOOL_DECL',
+  '\n': 'WHITESPACE',
+  '{': 'L_BRACE',
+  '}': 'R_BRACE',
+  '$': 'EOP'
 }
 
 
@@ -31,49 +51,89 @@ var lexer_index = {
 
 
 function lexer(input) {
-  var result = '';
+  document.querySelector('#output').innerHTML = ''; //Clear Output
+  var err = [];
+
   console.log(input)
   input = input.toLowerCase();
-  input = input.replace(/\s/g, '');
+  //input = input.replace(/\s/g, '');
 
   var bestCandidate = {
-    priority: 0,
+    priority: -1,
     value: '',
+    col: 0,
+    row: 0,
     endIndex: 0
   }
 
-  var programs = input.split('$'); //Divide up the programs
-  programs.forEach((p, i) => { //Row = programs[index] + 1
-    programs[i] += '$';
+  var programs = [];
+  var temp = input.split('$'); //Divide up the programs
+  console.log(temp)
+  temp.forEach((t, i) => {
+    if(t != '') {
+      t += '$';
+      programs.push(t);
+    }
   });
+  console.log(programs)
 
+  var row = 0;
   programs.forEach((p, id) => {
     var start = 0;
     var end = 0;
+    var col = 0;
     var currentString;
 
-    output(`INFO LEXER - Lexing program ${id}...`)
-    while(start <= p.length) {
-      while(end <= p.length) {
-        currentString = p.substring(start, end+1);
-        Object.keys(lexer_syntax).forEach((key) => {
-          if(currentString.match(lexer_syntax[key].values)) { //If a pattern is identified
-            if(bestCandidate.priority < lexer_syntax[key].priority) { //Update bestCandidate to alwasy have highest priority possible
-              bestCandidate.priority = lexer_syntax[key].priority;
-              bestCandidate.value = currentString;
-              bestCandidate.endIndex = end;
-            }
-          }
-        });
+
+
+    console.log('PROGRAM: ' + id)
+    output(`INFO LEXER - Lexing program ${id}...`);
+    while(start < p.length) {
+      if(p[start] == '\n') {
+        //console.log(`WHITESPACE at ${row}`);
+        row++;
+        col = 0;
         end++;
-        //console.log(start, end);
+      }else {
+        while(end <= p.length) {
+          currentString = p.substring(start, end+1);
+          console.log(currentString)
+
+          var didUpdate = false;
+          Object.keys(lexer_syntax).forEach((key) => {
+            if(currentString.match(lexer_syntax[key].values)) { //If a pattern is identified
+              if(bestCandidate.priority < lexer_syntax[key].priority) { //Update bestCandidate to alwasy have highest priority possible
+                bestCandidate.priority = lexer_syntax[key].priority;
+                bestCandidate.value = currentString;
+                bestCandidate.col = col;
+                bestCandidate.row = row;
+                bestCandidate.endIndex = end;
+                didUpdate = true;
+
+              }
+            }
+          });
+          if(didUpdate) {
+            start = bestCandidate.endIndex;
+          }
+          end++;
+        }
+        col++;
+        if(bestCandidate.priority > 0) {
+          createToken(bestCandidate);
+        }
+
+
       }
-      createToken(bestCandidate);
-      start++;
+      start++; //Start looking for new keyphrase
+      //start = bestCandidate.endIndex;
       end = start;
+      //start = end;
       bestCandidate = { //Reset bestCandidate
-        priority: 0,
+        priority: -1,
         value: '',
+        col: 0,
+        row: 0,
         endIndex: 0
       }
     }
@@ -83,8 +143,9 @@ function lexer(input) {
 
 }
 
-
+var tokens = [];
 function createToken(bestCandidate) {
+  output(`\tDEBUG LEXER - ${lexer_index[bestCandidate.value]} found at (${bestCandidate.row}:${bestCandidate.col})`)
   console.log(bestCandidate);
 }
 
