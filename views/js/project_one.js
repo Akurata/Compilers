19fd7fd8c5301d2700a71d80fe3bb15b48d38954
akurata <alexander.kurata1@marist.edu>
2019-09-19 16:39:20 -0400

    helper functions + continue work on lexing indv programs

--- views/js/project_one.js
+++ views/js/project_one.js
@@ -2,25 +2,31 @@
 var lexer_syntax = {
   keywords: {
     priority: 5,
-    values: /\bprint|\bwhile|\bif|\bint|\bstring|\bboolean|\bfalse|\btrue/g
+    values: /\b^print$|\b^while$|\b^if$|\b^int$|\b^string$|\b^boolean$|\b^false$|\b^true$/g
   },
   id: {
     priority: 4,
-    values: /[A-z]{1}/
+    values: /^[A-z]{1}$/
   },
   symbol: {
     priority: 3,
-    values: /\{|\}|\(|\)|\"|\=|\!|\+|\$|\/|\*/
+    values: /^\{$|^\}$|^\($|^\)$|^\"$|^\=$|^\!$|^\+$|^\$$|^\/$|^\*$/
   },
   digit: {
     priority: 2,
-    values: /[0-9]/
+    values: /^[0-9]$/
   },
   char: {
     priority: 1,
-    values: /[A-z]/
+    values: /^[A-z]$/
   }
 }
+
+var lexer_index = {
+
+}
+
+
 
 
 
@@ -29,57 +35,63 @@ function lexer(input) {
   console.log(input)
   input = input.toLowerCase();
   input = input.replace(/\s/g, '');
-  var currentChar = 0;
-  var currentString = input.substring(0, currentChar + 1);
 
   var bestCandidate = {
     priority: 0,
-    value: ''
+    value: '',
+    endIndex: 0
   }
 
   var programs = input.split('$'); //Divide up the programs
   programs.forEach((p, i) => { //Row = programs[index] + 1
     programs[i] += '$';
   });
-  console.log(programs)
 
-  //while(input[currentChar] != '$') {
+  programs.forEach((p, id) => {
+    var start = 0;
+    var end = 0;
+    var currentString;
 
-
-
-
-
-
-    Object.keys(lexer_syntax).forEach((key) => {
-      if(currentString.match(lexer_syntax[key].values)) { //If a pattern is identified
-        if(bestCandidate.priority < lexer_syntax[key].priority) { //Update bestCandidate to alwasy have highest priority possible
-          bestCandidate.priority = lexer_syntax[key].priority;
-          bestCandidate.value = currentString;
-        }
+    output(`INFO LEXER - Lexing program ${id}...`)
+    while(start <= p.length) {
+      while(end <= p.length) {
+        currentString = p.substring(start, end+1);
+        Object.keys(lexer_syntax).forEach((key) => {
+          if(currentString.match(lexer_syntax[key].values)) { //If a pattern is identified
+            if(bestCandidate.priority < lexer_syntax[key].priority) { //Update bestCandidate to alwasy have highest priority possible
+              bestCandidate.priority = lexer_syntax[key].priority;
+              bestCandidate.value = currentString;
+              bestCandidate.endIndex = end;
+            }
+          }
+        });
+        end++;
+        //console.log(start, end);
       }
-    });
-  //}
+      createToken(bestCandidate);
+      start++;
+      end = start;
+      bestCandidate = { //Reset bestCandidate
+        priority: 0,
+        value: '',
+        endIndex: 0
+      }
+    }
+
+  });
 
 
-  input = input.split('');
-
-  console.log(findBestCandidate());
-
-
-  result += input;
-
-  //Output results;
-  document.querySelector('#output').value = result;
-}
-
-function findBestCandidate(string, bestCandidate) {
-
-  return bestCandidate;
 }
 
 
+function createToken(bestCandidate) {
+  console.log(bestCandidate);
+}
 
 
+function output(info) {
+  document.querySelector('#output').innerHTML += `\n${info}`;
+}
 
 
 function start() {
